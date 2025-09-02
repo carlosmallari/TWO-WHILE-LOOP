9747670921b822eb083031054e38de091a760391
carlosmallari <carlosmallari1227@gmail.com>
2025-09-02 19:59:47 +0800

    Add files via upload

---
+++ TEST.cpp
@@ -0,0 +1,19 @@
+    #include <iostream>
+    using namespace std;
+
+    int main() {
+
+        int i = 0;
+        while (i <= 10) {
+            cout << i << endl;
+            i++;
+        }
+
+        int j = 10;
+        while (j >= 0) {
+            cout << j << endl;
+            j--;
+        }
+
+        return 0;
+    }
